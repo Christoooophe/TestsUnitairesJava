7d4611bc1077e8df28f8c484d3efebfee7d1821c
Christoooophe <christophealfier02@gmail.com>
2024-01-04 11:23:41 +0100

    :recycle: Modification des méthodes de la classe Salarie

--- src/main/java/fr/christophe/exercices/tp2/salarie/Salarie.java
+++ src/main/java/fr/christophe/exercices/tp2/salarie/Salarie.java
@@ -5,9 +5,8 @@ import fr.christophe.exercices.tp2.identite.Identite;
 import fr.christophe.exercices.tp2.salaire.Salaire;
 
 public abstract class Salarie {
-    private Salaire salaire;
-    private Identite identite;
-    public final int HEURES_TRAVAILLEES = 151;
+    protected Salaire salaire;
+    protected Identite identite;
     public Salarie(Salaire salaire, Identite identite){
         this.salaire = salaire;
         this.identite = identite;
@@ -29,12 +28,40 @@ public abstract class Salarie {
         this.identite = identite;
     }
 
-    public abstract void demenager(Adresse adresse);
-
-    public int travailler(){
-        return this.HEURES_TRAVAILLEES;
+    /**
+     * Permet de changer l'adresse du salarié
+     * @param adresse
+     */
+    public void demenager(Adresse adresse){
+        this.identite.setAdresse(adresse);
     }
 
-    public abstract int augmenter(int pourcentage);
+    /**
+     * Permet de retourner le nombre d'heures travaillées par le salarié
+     * @return heuresTravaillées
+     */
+    public String travailler(){
+        return "Le salarié " + this.identite.getNom() + " " + this.identite.getPrenom()
+                + " a travaillé 151.67 heures et a perçu " + this.salaire.payer(151.67);
+    }
+
+    /**
+     * Permet de retourner les heures travaillées mais avec un parametre (heuresTravaillees)
+     * @param heuresTravaillees
+     * @return heures travaillées
+     */
+    public String travailler(double heuresTravaillees){
+        return "Le salarié " + identite.getNom() + " " + identite.getPrenom() + " a travaillé "
+                + heuresTravaillees + " heures et a perçu " + salaire.payer(heuresTravaillees);
+    }
+
+    /**
+     * Permet d'augmenter le tauxHoraire du salarié en passant un pourcentage en paramètres et en faisant un set du
+     * taux horaire dans la classe Salaire
+     * @param pourcentage
+     */
+    public void augmenter(double pourcentage){
+        this.salaire.setTauxHoraire(this.salaire.getTauxHoraire() * pourcentage);
+    }
 
 }
